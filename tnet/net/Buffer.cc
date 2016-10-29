ce8c19db0d8e711a3c377732d3cd095ff98bde16
taochunyu <tao_chunyu@icloud.com>
2016-10-29 21:22:44 +0800

    Buffer

--- tnet/net/Buffer.cc
+++ tnet/net/Buffer.cc
@@ -1,4 +1,8 @@
 #include <tnet/net/Buffer.h>
+#include <tnet/net/SocketsOps.h>
+
+#include <errno.h>
+#include <sys/uio.h>
 
 using namespace tnet;
 using namespace tnet::net;
@@ -13,3 +17,25 @@ using namespace tnet::net;
 /// |                   |                  |                  |
 /// 0      <=      readerIndex   <=   writerIndex    <=     size
 /// @endcode
+
+int Buffer::readFdIntoBuffer(int fd, int* savedError) {
+  char extrabuf[65536];
+  struct iovec vec[2];
+  const size_t writable = writableBytes();
+  vec[0].iov_base = begin() + writable;
+  vec[0].iov_len = writable;
+  vec[1].iov_base = extrabuf;
+  vec[1].iov_len = sizeof(extrabuf);
+
+  const int iovcnt = (writable < sizeof(extrabuf)) ? 2 : 1;
+  const ssize_t n = sockets::readv(fd, vec, iovcnt);
+  if (n < 0) {
+    *savedError = errno;
+  } else if (implicit_cast<size_t>(n) <= writable) {
+    _writerIndex += n;
+  } else {
+    _writerIndex = _buffer.size();
+    append(extrabuf, n - writable);
+  }
+  return n;
+}
--- tnet/net/TcpServer.h
+++ tnet/net/TcpServer.h
@@ -1,5 +1,40 @@
 #ifndef TNET_NET_TCPSERVER_H
 #define TNET_NET_TCPSERVER_H
 
+#include <tnet/base/nocopyable.h>
+#include <map>
+#include <string>
+#include <memory>
+
+namespace tnet {
+namespace net {
+
+class InetAddress;
+class Acceptor;
+class EventLoop;
+class EventLoopThreadPool;
+class TcpConnection;
+
+class TcpServer : tnet::nocopyable {
+ public:
+  using TcpConnectionPtr = std::shared_ptr<TcpConnection>;
+  using ThreadInitCallback = std::function<void(EventLoop*)>;
+  enum Option { kNoReusePort, kReusePort };
+
+  TcpServer(EventLoop* loop,
+            const InetAddress& listenAddr,
+            const std::string& nameArg,
+            Option opt = kNoReusePort);
+  ~TcpServer();
+  const std::string ipPort() const { return _ipPort; }
+  const std::string name() const { return _name; }
+  EventLoop* getLoop() const { return _loop; }
+
+   
+ private:
+};
+
+}  // namespace net
+}  // namespace tnet
 
 #endif  // TNET_NET_TCPSERVER_H
